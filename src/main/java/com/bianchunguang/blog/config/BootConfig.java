71c12c4f95bb4c9969f1e629ae16534604f04087
cnbcg <admin@bianchunguang.com>
2016-03-02 01:06:26 +0800

    绑定域名

--- src/main/java/com/bianchunguang/blog/config/BootConfig.java
+++ src/main/java/com/bianchunguang/blog/config/BootConfig.java
@@ -6,11 +6,18 @@ import com.bianchunguang.blog.persistence.services.SystemConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.util.StringUtils;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 
@@ -22,6 +29,12 @@ public class BootConfig implements EmbeddedServletContainerCustomizer {
 
     @Override
     public void customize(ConfigurableEmbeddedServletContainer container) {
+        try {
+            container.setAddress(InetAddress.getByName("www.bianchunguang.com"));
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException("域名无效:" + e.getMessage());
+        }
         systemConfigService.save(Arrays.asList(Constants.SystemConfigConstants.values()).stream().filter(config -> !systemConfigService.exist(config.toString()))
                 .map(config -> {
                     String name = config.toString(), value = environment.getProperty(name);
